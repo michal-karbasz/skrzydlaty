58bf9e05ea36cbb2f79d9d2b9722a8eee4d6c7a1
michalkarbasz <michal.karbasz@gmail.com>
2019-08-14 01:40:44 +0200

    Updates

---
+++ precache-manifest.7ade58ebb3002f381aea10e80c3af54d.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5936db8fe9537849c7707aa7c8cbd1e",
+    "url": "/skrzydlaty/index.html"
+  },
+  {
+    "revision": "bed735fa49b11e3d8b3f",
+    "url": "/skrzydlaty/static/css/2.0ee4b489.chunk.css"
+  },
+  {
+    "revision": "bed735fa49b11e3d8b3f",
+    "url": "/skrzydlaty/static/js/2.f53a2224.chunk.js"
+  },
+  {
+    "revision": "8227ca9ef3efb5ecdb09",
+    "url": "/skrzydlaty/static/js/main.f5f7acb5.chunk.js"
+  },
+  {
+    "revision": "a7ce7ea340415670d7c9",
+    "url": "/skrzydlaty/static/js/runtime~main.91eab71c.js"
+  },
+  {
+    "revision": "2335846c07f9009a24acfddd6167cc37",
+    "url": "/skrzydlaty/static/media/1.2335846c.JPG"
+  },
+  {
+    "revision": "dc55fed84aa073b3b40efc3d966bead6",
+    "url": "/skrzydlaty/static/media/1.dc55fed8.JPG"
+  },
+  {
+    "revision": "849e3fd025a1107bc06b82860de9c3dc",
+    "url": "/skrzydlaty/static/media/10.849e3fd0.JPG"
+  },
+  {
+    "revision": "df8bd34ae4db9e814312b0cde3bffa35",
+    "url": "/skrzydlaty/static/media/10.df8bd34a.JPG"
+  },
+  {
+    "revision": "4ec549b86a064883ba49d6eb6e44d61e",
+    "url": "/skrzydlaty/static/media/11.4ec549b8.JPG"
+  },
+  {
+    "revision": "679045ecc6b0f5901119f833eacbd16d",
+    "url": "/skrzydlaty/static/media/11.679045ec.JPG"
+  },
+  {
+    "revision": "59aeb9d879da313fb0c7d61d655cb295",
+    "url": "/skrzydlaty/static/media/12.59aeb9d8.JPG"
+  },
+  {
+    "revision": "e48f7842bfcdc72bf7bf250ee3d20a57",
+    "url": "/skrzydlaty/static/media/12.e48f7842.JPG"
+  },
+  {
+    "revision": "bfdabd8d69b5c9bf2d4eb30932969ae5",
+    "url": "/skrzydlaty/static/media/13.bfdabd8d.JPG"
+  },
+  {
+    "revision": "d59a3b8f0b31aa41a5ef247b1dfb70fb",
+    "url": "/skrzydlaty/static/media/13.d59a3b8f.JPG"
+  },
+  {
+    "revision": "8f0daad82b591b98c71a9a49eec8feb3",
+    "url": "/skrzydlaty/static/media/14.8f0daad8.JPG"
+  },
+  {
+    "revision": "a8e21b26eaea073abfecbad5dc4baeea",
+    "url": "/skrzydlaty/static/media/14.a8e21b26.JPG"
+  },
+  {
+    "revision": "4aec795d745746b225727cc9975b5892",
+    "url": "/skrzydlaty/static/media/15.4aec795d.JPG"
+  },
+  {
+    "revision": "e6e6386188ef4d214cbbe9b4074c5025",
+    "url": "/skrzydlaty/static/media/15.e6e63861.JPG"
+  },
+  {
+    "revision": "4a354a39bac1a8740d4c76f980a1aefc",
+    "url": "/skrzydlaty/static/media/16.4a354a39.JPG"
+  },
+  {
+    "revision": "d9169524bfc00bfd513ea7dd4d27bcf3",
+    "url": "/skrzydlaty/static/media/16.d9169524.JPG"
+  },
+  {
+    "revision": "a19ee6e1ae9065cb14e4725b556735be",
+    "url": "/skrzydlaty/static/media/17.a19ee6e1.JPG"
+  },
+  {
+    "revision": "aa436c15b18662962e5b280fb0dfbbd4",
+    "url": "/skrzydlaty/static/media/17.aa436c15.JPG"
+  },
+  {
+    "revision": "12253d59358423458ed0e2972612b861",
+    "url": "/skrzydlaty/static/media/18.12253d59.JPG"
+  },
+  {
+    "revision": "edd40f2011c14fca1d04c53867158957",
+    "url": "/skrzydlaty/static/media/18.edd40f20.JPG"
+  },
+  {
+    "revision": "98c7f670647a08ac311fed7845d4ec55",
+    "url": "/skrzydlaty/static/media/19.98c7f670.JPG"
+  },
+  {
+    "revision": "be089e62a0c88f04d0054734baf6c021",
+    "url": "/skrzydlaty/static/media/19.be089e62.JPG"
+  },
+  {
+    "revision": "674b712a2fa38c33ce0f770593d7b717",
+    "url": "/skrzydlaty/static/media/2.674b712a.JPG"
+  },
+  {
+    "revision": "bb21b01fb7e4f04a0d94fca6239110e1",
+    "url": "/skrzydlaty/static/media/2.bb21b01f.JPG"
+  },
+  {
+    "revision": "854ccf2d64827c22f9ff4702b16fbb6e",
+    "url": "/skrzydlaty/static/media/20.854ccf2d.JPG"
+  },
+  {
+    "revision": "f7d94e790b64b3af6ebc3d78b551eef8",
+    "url": "/skrzydlaty/static/media/20.f7d94e79.JPG"
+  },
+  {
+    "revision": "4c2a9f43350da3b15f0b8c47490bd32f",
+    "url": "/skrzydlaty/static/media/21.4c2a9f43.JPG"
+  },
+  {
+    "revision": "bdccb4d8ad72fadb3238de5fe65cbfd8",
+    "url": "/skrzydlaty/static/media/21.bdccb4d8.JPG"
+  },
+  {
+    "revision": "56f4fc20f1fa6a42c85e5426edca62ce",
+    "url": "/skrzydlaty/static/media/22.56f4fc20.JPG"
+  },
+  {
+    "revision": "7ff220a378a286182ad030587e22dc06",
+    "url": "/skrzydlaty/static/media/22.7ff220a3.JPG"
+  },
+  {
+    "revision": "cf5dc14eeac51d594f5bf32de28b7227",
+    "url": "/skrzydlaty/static/media/23.cf5dc14e.JPG"
+  },
+  {
+    "revision": "f10757658ddf20a3c1ae500ef5207301",
+    "url": "/skrzydlaty/static/media/23.f1075765.JPG"
+  },
+  {
+    "revision": "31ccc21c6e0bae19fdc96e0cf7cab94d",
+    "url": "/skrzydlaty/static/media/24.31ccc21c.JPG"
+  },
+  {
+    "revision": "43af2f30366f38eeda14b055f017fc18",
+    "url": "/skrzydlaty/static/media/24.43af2f30.JPG"
+  },
+  {
+    "revision": "5243add700a234e2019a15fc895ec649",
+    "url": "/skrzydlaty/static/media/3.5243add7.JPG"
+  },
+  {
+    "revision": "cb79c37345158fc8bb0ca2e3e0072b3a",
+    "url": "/skrzydlaty/static/media/3.cb79c373.JPG"
+  },
+  {
+    "revision": "464c00e8462d8e6e1108e86ed51954e5",
+    "url": "/skrzydlaty/static/media/4.464c00e8.JPG"
+  },
+  {
+    "revision": "b7301972b9bf3ff44bf26965a2305e7c",
+    "url": "/skrzydlaty/static/media/4.b7301972.JPG"
+  },
+  {
+    "revision": "0da97a24cbb5e1620a3b8d6c3673d5e5",
+    "url": "/skrzydlaty/static/media/5.0da97a24.JPG"
+  },
+  {
+    "revision": "8e07621e056d3ef9f285112998509f51",
+    "url": "/skrzydlaty/static/media/5.8e07621e.JPG"
+  },
+  {
+    "revision": "3a5aff5affb0171441a33f65baf3e560",
+    "url": "/skrzydlaty/static/media/6.3a5aff5a.JPG"
+  },
+  {
+    "revision": "ed843b446b44c1101e7f7806e4e02ba1",
+    "url": "/skrzydlaty/static/media/6.ed843b44.JPG"
+  },
+  {
+    "revision": "7bfdcf320210741d0fe78da65679f07f",
+    "url": "/skrzydlaty/static/media/7.7bfdcf32.JPG"
+  },
+  {
+    "revision": "b0379003a10b5be61797fbbec5c3fa51",
+    "url": "/skrzydlaty/static/media/7.b0379003.JPG"
+  },
+  {
+    "revision": "e8e94f06201246b8fbb97659a41c3ec3",
+    "url": "/skrzydlaty/static/media/8.e8e94f06.JPG"
+  },
+  {
+    "revision": "f0395db32f85c7bc04ad4063ae7e475c",
+    "url": "/skrzydlaty/static/media/8.f0395db3.JPG"
+  },
+  {
+    "revision": "706cd5246e33a3435ebc659f9f0cf027",
+    "url": "/skrzydlaty/static/media/9.706cd524.JPG"
+  },
+  {
+    "revision": "93b333bb19c065849e2cf45768c3860d",
+    "url": "/skrzydlaty/static/media/9.93b333bb.JPG"
+  },
+  {
+    "revision": "209ea8cda309151772ca9bdfc981229e",
+    "url": "/skrzydlaty/static/media/about_icon.209ea8cd.jpeg"
+  },
+  {
+    "revision": "15c58890eac6c14b151092be316d233a",
+    "url": "/skrzydlaty/static/media/art_workshops.15c58890.jpg"
+  },
+  {
+    "revision": "0f407d1ef5eec95a689f178821e7b8a2",
+    "url": "/skrzydlaty/static/media/birthday_icon.0f407d1e.jpeg"
+  },
+  {
+    "revision": "902a63c1d98de97438ce2ef95877d5c4",
+    "url": "/skrzydlaty/static/media/border_ornament_horizontal.902a63c1.jpeg"
+  },
+  {
+    "revision": "ecb96d5c1e436a295ca829ebd9ffecf6",
+    "url": "/skrzydlaty/static/media/gallery_icon.ecb96d5c.jpeg"
+  },
+  {
+    "revision": "b47d50cd9a5c1063b0d4a7f2b9d6036a",
+    "url": "/skrzydlaty/static/media/home_icon.b47d50cd.jpeg"
+  },
+  {
+    "revision": "420064d004b55669c2cfd996b93bffff",
+    "url": "/skrzydlaty/static/media/kasia.420064d0.jpg"
+  },
+  {
+    "revision": "0ece6513efe6b0e85aca2a467fe186a2",
+    "url": "/skrzydlaty/static/media/skrzydlate_logo.0ece6513.jpeg"
+  },
+  {
+    "revision": "da47390cfbcdc8f1f5dd1ec1606945b2",
+    "url": "/skrzydlaty/static/media/sprite_long.da47390c.png"
+  },
+  {
+    "revision": "a9f40da919a87970eee1530555322b15",
+    "url": "/skrzydlaty/static/media/sprite_small.a9f40da9.png"
+  },
+  {
+    "revision": "b43bd1edd079d2a8a619ada064b16996",
+    "url": "/skrzydlaty/static/media/subscribe_icon.b43bd1ed.jpeg"
+  },
+  {
+    "revision": "ba5ac8769d01f65434f71e616b883b83",
+    "url": "/skrzydlaty/static/media/wieniec.ba5ac876.jpeg"
+  },
+  {
+    "revision": "9f4b21cf905479c623cecb8d37dc74ad",
+    "url": "/skrzydlaty/static/media/workshops_icon.9f4b21cf.jpeg"
+  }
+]);
